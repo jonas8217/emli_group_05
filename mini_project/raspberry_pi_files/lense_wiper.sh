3836c1aeccdc356771258753dcf119c7fe43f94b
emli052024rpi <emli052024rpi@gmail.com>
2024-05-22 16:16:30 +0200

    fix of meta_data.sh after accidental deletion, began making lense_wiper.sh

--- mini_project/raspberry_pi_files/lense_wiper.sh
+++ mini_project/raspberry_pi_files/lense_wiper.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-mosquitto_sub -h localhost -p 1883 -u emli05 -P emli05 -t emli05/ |
+mosquitto_sub -h localhost -p 1883 -u emli05 -P emli05 -t emli05/wiper_angle |
    while read payload ; do
-      echo "test"
+      echo payload
    done
--- mini_project/raspberry_pi_files/meta_data.sh
+++ mini_project/raspberry_pi_files/meta_data.sh
@@ -1,30 +1,31 @@
 #!/bin/bash
 
 # get important metadata
-# variable 1 = trigger, variable 2 = time, variable 3 = date
+# variable 1 = trigger, variable 2 = time, variable 3 = date, variable 4 = epoch
 metadata=$(exiftool -EXIF:ISO -EXIF:ExposureTime -EXIF:SubjectDistance -s3 "/var/www/html/database/images/$3/$2.jpg")
 iso=$(echo "${metadata}" | sed -n 1p) && exposure_time=$(echo "${metadata}" | sed -n 2p) && subject_distance=$(echo "${metadata}" | sed -n 3p)
 
-# Read trigger version
-trigger="$1"
+# read camera ID
+read -r ID<"/var/www/html/database/camera_id.txt"
 
 # Make JSON file
 json_file=$(cat <<EOF
 {
-        "File Name": "${current_time}",
-        "Create Date": "${current_date}",
-        "Create Seconds Epoch": "${epoch_time}",
-        "Trigger": "${trigger}",
+        "File Name": "$2",
+        "Create Date": "$3",
+        "Create Seconds Epoch": "$4",
+        "Trigger": "$1",
         "Subject Distance": "${subject_distance}",
         "Exposure Time": "${exposure_time}",
-        "ISO": "${iso}"
+        "ISO": "${iso}",
+	"Camera ID" : "${ID}",
+	"Drone ID" : ""
 }
 EOF
 )
 
 # Save file
-echo $json_file > "/var/www/html/database/images/${current_date}/${current_time}".json
-
+echo $json_file > "/var/www/html/database/images/$3/$2".json
 
 
 
