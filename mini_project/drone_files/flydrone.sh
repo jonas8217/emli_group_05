6a7071d0304070477d05841e53c5f4ba06191356
Anders-S-M <andma20@student.sdu.dk>
2024-05-23 17:12:21 +0200

    Done for today

--- mini_project/drone_files/flydrone.sh
+++ mini_project/drone_files/flydrone.sh
@@ -5,8 +5,10 @@ REMOTE_USER='emli'
 REMOTE_PASSWORD='05050505'
 REMOTE_IP='192.168.10.1'
 REMOTE_FOLDER='/var/www/html/database/images'
-LOCAL_FOLDER='/home/anders/Desktop/emli-final/temp'
-Drone_ID="777"
+LOCAL_FOLDER='emli_group_05/mini_project/drone_files/temp'
+# get drone ID
+read -r ID<"/home/anders/Desktop/emli-final/emli_group_05/mini_project/drone_files/drone_id.txt"
+Drone_ID=${ID}
 WIFI_INTERFACE=wlp3s0
 SPECIFIC_KEY="Drone ID"
 
@@ -28,6 +30,12 @@ copy_file_via_scp() {
     scp -o StrictHostKeyChecking=no "${REMOTE_USER}@${REMOTE_IP}:${remote_path}" "${local_path}"
 }
 
+# Post to SQL DB
+WIFI_log() {
+    #echo "Logging Wifi" 
+    ./emli_group_05/mini_project/drone_files/wifi_logger.sh
+}
+
 # Function to copy files with specific key
 copy_files_with_specific_key() {
     # Find all JSON files in the remote folder and its subdirectories
@@ -40,7 +48,7 @@ copy_files_with_specific_key() {
         copy_file_via_scp "$remote_json_file" "$local_json_file"
         
         # Write signal quality measures to SQL database
-        ./wifi_logger.sh
+        WIFI_log
 
         # Extract the value of the Drone ID key using jq
         value=$(jq -r '.["Drone ID"]' "$local_json_file")
@@ -75,7 +83,7 @@ copy_files_with_specific_key() {
     done
 
     # Move all new images and their .json's to the permanent storage folder pre AI
-    mv "$LOCAL_FOLDER"/* "images"/
+    mv "$LOCAL_FOLDER"/* "emli_group_05/mini_project/drone_files/images"/
 }
 
 while true; do
@@ -98,7 +106,7 @@ while true; do
         sshpass -p "$REMOTE_PASSWORD" ssh -o StrictHostKeyChecking=no "${REMOTE_USER}@${REMOTE_IP}" "./emli_group_05/mini_project/raspberry_pi_files/update_time.sh ${time}"
         
         # Write signal quality measures to SQL database
-        ./wifi_logger.sh
+        WIFI_log
 
         # Extract image and synchronize
         {
@@ -112,7 +120,7 @@ while true; do
         else
 
         # Try to connect
-        echo "Connecting to '$wifi_ssid'."
+        #echo "Connecting to '$wifi_ssid'."
 
         # Disconnect from any active WiFi connection
         nmcli con down id "$connected_ssid" >/dev/null 2>&1
@@ -126,4 +134,6 @@ while true; do
         echo "SSID ${SSID_TO_FIND} not found. Retrying in 30 seconds..."
         sleep 30 
     fi
+    sleep 5
+    break
 done
